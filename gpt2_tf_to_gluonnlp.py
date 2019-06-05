0b30b0e9364c336d579ad83b633871dc229aaf9e
sxjscience <sxjscience001@gmail.com>
2019-06-05 12:28:58 +0800

    Update gpt2_tf_to_gluonnlp.py

--- gpt2_tf_to_gluonnlp.py
+++ gpt2_tf_to_gluonnlp.py
@@ -1,12 +1,13 @@
 import os
 import io
 import json
+import argparse
 from gluonnlp.vocab import Vocab
 
-def convert_vocab_bpe(src_path, dst_path):
-    if not os.path.exists(dst_path):
-        os.makedirs(dst_path)
-    with io.open(os.path.join(src_path, 'encoder.json'), 'r', encoding='utf-8') as f:
+def convert_vocab_bpe(src_dir, dst_dir):
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    with io.open(os.path.join(src_dir, 'encoder.json'), 'r', encoding='utf-8') as f:
         token_to_idx = json.load(f)
         token_to_idx = {k : int(v) for k, v in token_to_idx.items()}
     idx_to_token = {v: k for k, v in token_to_idx.items()}
@@ -17,9 +18,17 @@ def convert_vocab_bpe(src_path, dst_path):
     vocab._padding_token = None
     vocab._bos_token = None
     vocab._eos_token = '<|endoftext|>'
-    with io.open(os.path.join(dst_path, 'vocab.json'), 'w', encoding='utf-8') as of:
+    with io.open(os.path.join(dst_dir, 'vocab.json'), 'w', encoding='utf-8') as of:
         of.write(vocab.to_json())
-    with io.open(os.path.join(src_path, 'vocab.bpe'), 'r', encoding='utf-8') as f:
-        of = io.open(os.path.join(dst_path, 'bpe_ranks.json'), 'w', encoding='utf-8')
+    with io.open(os.path.join(src_dir, 'vocab.bpe'), 'r', encoding='utf-8') as f:
+        of = io.open(os.path.join(dst_dir, 'bpe_ranks.json'), 'w', encoding='utf-8')
         of.write(f.read())
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--src_dir", help="Source path of the model directory in openai/gpt-2", type=str, required=True)
+parser.add_argument("--dst_dir", help="Destination path of the model directory of gluonnlp", type=str, required=True)
+
+args = parser.parse_args()
+print('Convert {} to {}'.format(args.src_dir, args.dst_dir))
+convert_vocab_bpe(args.src_dir, args.dst_dir)
