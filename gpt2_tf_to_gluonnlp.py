427d638837f1e1957d63690662d21ad15f9f7ef3
sxjscience <sxjscience001@gmail.com>
2019-06-05 14:36:31 +0800

    Update gpt2_tf_to_gluonnlp.py

--- gpt2_tf_to_gluonnlp.py
+++ gpt2_tf_to_gluonnlp.py
@@ -2,7 +2,23 @@ import os
 import io
 import json
 import argparse
+import mxnet as mx
+import re
 from gluonnlp.vocab import Vocab
+from model import GPT2_117M, GPT2_345M
+
+
+def read_tf_checkpoint(path):
+    """read tensorflow checkpoint"""
+    from tensorflow.python import pywrap_tensorflow
+    tensors = {}
+    reader = pywrap_tensorflow.NewCheckpointReader(path)
+    var_to_shape_map = reader.get_variable_to_shape_map()
+    for key in sorted(var_to_shape_map):
+        tensor = reader.get_tensor(key)
+        tensors[key] = tensor
+    return tensors
+
 
 def convert_vocab_bpe(src_dir, dst_dir):
     if not os.path.exists(dst_dir):
@@ -25,11 +41,64 @@ def convert_vocab_bpe(src_dir, dst_dir):
         of = io.open(os.path.join(dst_dir, 'bpe_ranks.json'), 'w', encoding='utf-8')
         of.write(f.read())
 
+def convert_tf_param(gluon_model, tf_ckpt_path, gluon_param_save_path):
+    TF_GLUON_NAME_MAP = {
+        'model/wte' : '_embed.weight',
+        'model/wpe' : '_pos_embed.weight',
+        'model/h(\d+)/ln_1/b' : '_attn_ln.{}.beta',
+        'model/h(\d+)/ln_1/g' : '_attn_ln.{}.gamma',
+        'model/h(\d+)/ln_2/b' : '_ffn_ln.{}.beta',
+        'model/h(\d+)/ln_2/g' : '_ffn_ln.{}.gamma',
+        'model/h(\d+)/mlp/c_fc/w' : '_ffn_layers.{}._hidden_map.weight',
+        'model/h(\d+)/mlp/c_fc/b' : '_ffn_layers.{}._hidden_map.bias',
+        'model/h(\d+)/mlp/c_proj/w' : '_ffn_layers.{}._out_map.weight',
+        'model/h(\d+)/mlp/c_proj/b' : '_ffn_layers.{}._out_map.bias',
+        'model/h(\d+)/attn/c_attn/w' : '_self_attention_layers.{}._multi_head_qkv_proj.weight',
+        'model/h(\d+)/attn/c_attn/b' : '_self_attention_layers.{}._multi_head_qkv_proj.bias',
+        'model/h(\d+)/attn/c_proj/w' : '_self_attention_layers.{}._out_proj.weight',
+        'model/h(\d+)/attn/c_proj/b' : '_self_attention_layers.{}._out_proj.bias',
+        'model/ln_f/b' : '_final_ln.beta',
+        'model/ln_f/g' : '_final_ln.gamma'
+    }
+    tf_params = read_tf_checkpoint(tf_ckpt_path)
+    gluon_model.initialize()
+    out = gluon_model(mx.nd.zeros((1, 1)))
+    params = gluon_model._collect_params_with_prefix()
+    loaded = {k: False for k in params}
+    for name, param_value in tf_params.items():
+        gluon_param_name = None
+        for lhs, rhs in TF_GLUON_NAME_MAP.items():
+            match = re.match(lhs, name)
+            if match is not None:
+                if len(match.groups()) > 0:
+                    gluon_param_name = rhs.format(match.groups()[0])
+                    break
+                else:
+                    gluon_param_name = rhs
+        assert gluon_param_name is not None
+        params[gluon_param_name].set_data(mx.nd.array(param_value))
+        loaded[gluon_param_name] = True
+    for name in params:
+        if not loaded[name]:
+            print('{} is not loaded!'.format(name))
+    gluon_model.save_parameters(gluon_param_save_path)
+    mx.nd.waitall()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--src_dir", help="Source path of the model directory in openai/gpt-2", type=str, required=True)
 parser.add_argument("--dst_dir", help="Destination path of the model directory of gluonnlp", type=str, required=True)
+parser.add_argument('--model', help='The specific model we need to convert', type=str, choices=['117M', '345M'])
 
 args = parser.parse_args()
-print('Convert {} to {}'.format(args.src_dir, args.dst_dir))
-convert_vocab_bpe(args.src_dir, args.dst_dir)
+print('Convert {} to {}'.format(os.path.join(args.src_dir, args.model),
+                                os.path.join(args.dst_dir, args.model)))
+convert_vocab_bpe(os.path.join(args.src_dir, args.model),
+                  os.path.join(args.dst_dir, args.model))
+if args.model == '117M':
+    gluon_model = GPT2_117M()
+elif args.model == '345M':
+    gluon_model = GPT2_345M()
+else:
+    raise NotImplementedError
+convert_tf_param(gluon_model, os.path.join(args.src_dir, args.model, 'model.ckpt'),
+                 os.path.join(args.dst_dir, args.model, 'model.params'))
