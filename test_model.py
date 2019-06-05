35fa647d638e21281f094d7804d1aaccbc67d0f8
sxjscience <sxjscience001@gmail.com>
2019-06-05 12:20:07 +0800

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,5 +1,10 @@
+import mxnet as mx
 from model import GPT2_117M
 
 
-model = GPT2_117M()
-print(model)
+
+def test_pretrained_gpt2():
+    model = GPT2_117M()
+    model.initialize(ctx=mx.gpu())
+    model.hybridize()
+    print(model)
