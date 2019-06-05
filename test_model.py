5a6ebc7d45870518082d662cd309b6ad064b9977
sxjscience <sxjscience001@gmail.com>
2019-06-05 15:23:00 +0800

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -27,7 +27,7 @@ def test_pretrained_gpt2(ctx=None):
     indices = vocab[tokenizer(sentence)]
     nd_indices = mx.nd.expand_dims(mx.nd.array(indices, ctx=ctx), axis=0)
     logits, new_states = model(nd_indices, None)
-    npt.assert_allclose(logits.asnumpy(), gt_logits, 1E-2, 1E-2)
+    npt.assert_allclose(logits.asnumpy(), gt_logits, 5E-3, 5E-3)
 
 test_pretrained_gpt2(ctx=mx.gpu())
 
