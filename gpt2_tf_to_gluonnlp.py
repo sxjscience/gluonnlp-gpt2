43eda04b85f5375a5180c268ce7546f75ab45c44
sxjscience <sxjscience001@gmail.com>
2019-06-05 12:32:46 +0800

    fix

--- gpt2_tf_to_gluonnlp.py
+++ gpt2_tf_to_gluonnlp.py
@@ -10,7 +10,7 @@ def convert_vocab_bpe(src_dir, dst_dir):
     with io.open(os.path.join(src_dir, 'encoder.json'), 'r', encoding='utf-8') as f:
         token_to_idx = json.load(f)
         token_to_idx = {k : int(v) for k, v in token_to_idx.items()}
-    idx_to_token = {v: k for k, v in token_to_idx.items()}
+    idx_to_token = {int(v): k for k, v in token_to_idx.items()}
     vocab = Vocab(unknown_token=None)
     vocab._idx_to_token = idx_to_token
     vocab._token_to_idx = token_to_idx
